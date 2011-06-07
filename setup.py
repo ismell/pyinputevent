33696687adf6995c08e09ea0e16a50e4ea5068e3
Raul E Rangel <Raul.E.Rangel@gmail.com>
2011-06-06 20:30:41 -0600

    Moved files around and made an egg installer

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(
+    name='pyinputevent',
+    version='0.1',
+    packages=['pyinputevent',],
+    license='MIT license',
+    url='https://github.com/ismell/pyinputevent',
+    author='Robert M. Thomson',
+    author_email='git@corporatism.org',
+    maintainer='Raul E Rangel',
+    maintainer_email='ismell@ismell.org',
+    description='PyInputEvent provides a python interface to Linux\'s input subsystem'
+)
